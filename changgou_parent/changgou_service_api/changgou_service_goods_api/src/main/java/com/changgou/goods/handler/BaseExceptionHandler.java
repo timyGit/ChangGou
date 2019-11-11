a882871b5199bc52c74604e071ba307c86982c1c
timy <57529412@qq.com>
2019-11-11 23:52:46 +0800

    创建统一异常处理类,感觉比以前的方法聪明了好多

---
+++ changgou_parent/changgou_service_api/changgou_service_goods_api/src/main/java/com/changgou/goods/handler/BaseExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.changgou.goods.handler;
+
+import com.changgou.common.pojo.Result;
+import com.changgou.common.pojo.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 笔记说这是统一异常处理
+ */
+@ControllerAdvice
+public class BaseExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR,e.getMessage());
+    }
+}
